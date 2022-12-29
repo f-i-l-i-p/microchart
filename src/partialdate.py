eeec9d70fa685a8bfd6255be072842d43f6e2778
Filip Johnsson <johnsson.filip@protonmail.com>
2022-12-29 21:20:58 +0100

    Bugfixes and more date data

--- src/partialdate.py
+++ src/partialdate.py
@@ -1,9 +1,11 @@
-
 class PartialDate:
     """
     Class containing parts of a full date.
     """
-    def __init__(self, day: int, hour: int):
+
+    def __init__(self, year: int, month: int, day: int, hour: int):
+        self.year = year
+        self.month = month
         self.day = day
         self.hour = hour
         self.weekday = 0
@@ -25,20 +27,29 @@ class PartialDate:
         if day == 6:
             return "Sun"
 
+    def _to_comparable(self) -> int:
+        """
+        Returns an integer that can be used for comparing PartialDates.
+        """
+        return self.year * 12 * 31 * 24 + self.month * 31 * 24 + self.day * 24 + self.hour
+
     def is_same_hour(self, other: 'PartialDate') -> bool:
         """
         Returns True if a given PartialDate is during the same hour.
         """
-        return self.day == other.day and self.hour == other.hour
+        return self._to_comparable() == other._to_comparable()
 
     def is_in_range(self, before: 'PartialDate', after: 'PartialDate') -> bool:
         """
         Returns True if this PartialData is between two others.
         """
-        return before.day * 24 + before.hour <= self.day * 24 + self.hour <= after.day * 24 + after.hour
+        return self.more_or_equal_to(before) and self.less_or_equal_to(after)
 
     def more_or_equal_to(self, other: 'PartialDate'):
-        return self.day * 24 + self.hour >= other.day * 24 + other.hour
+        return self._to_comparable() >= other._to_comparable()
 
     def less_or_equal_to(self, other: 'PartialDate'):
-        return self.day * 24 + self.hour <= other.day * 24 + other.hour
+        return self._to_comparable() <= other._to_comparable()
+
+    def __str__(self) -> str:
+        return f"{self.year}, {self.month}, {self.day}, {self.hour}"
